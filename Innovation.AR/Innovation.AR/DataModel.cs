7490f25de5b7553d4968f2733f293179f26349b5
P0W <powprashant@gmail.com>
2017-11-15 17:04:24 +0530

    Added data model

---
+++ Innovation.AR/Innovation.AR/DataModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Innovation.AR
+{
+    public class DataModel
+    {
+        public DataModel()
+        {
+
+            uldEnabled = false;
+            casEnabled = false;
+            airdropLight = 0;
+            radarAltitude = "0 ft";
+            timeToDrop = "0.0 sec";
+            textToSpeech = "";
+
+            casMsgList = new List<string>();
+        }
+
+        public bool uldEnabled { get; set; }
+        public bool casEnabled { get; set; }
+        public int airdropLight { get; set; }
+
+        //public string masterTime { get; set;  }
+        public string radarAltitude { get; set; }
+
+        public string timeToDrop { get; set; }
+
+        public string textToSpeech { get; set; }
+        public List<string> casMsgList { get; set; }
+    }
+}
